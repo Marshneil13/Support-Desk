9d98461aac347b0d9817a4ae5cc32c9fc8a3cbbd
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-08-02 23:56:05 +0530

    Register Page with Redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom";
 import {toast} from 'react-toastify'
 import { FaUser } from "react-icons/fa";
 import {useSelector, useDispatch} from 'react-redux'
-import {register} from '../features/auth/authSlice'
+import {register, reset} from '../features/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,11 +16,23 @@ function Register() {
   const { name, email, password, password2 } = formData;
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const {user, isLoading, isSuccess, message} = 
+  const {user, isLoading, isError, isSuccess, message} = 
   useSelector(
       (state) => state.auth
   )
+
+  useEffect(() => {
+      if(isError){
+          toast.error(message)
+      }
+      if(isSuccess || user){
+          navigate('/')
+      }
+
+      dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
       setFormData((prevState) => ({
