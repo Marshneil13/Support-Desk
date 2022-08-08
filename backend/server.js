5dbd5422a521d45eb8f5d18edf9c2762352a31a7
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-08-08 16:33:50 +0530

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const connectDB = require('./config/db')
@@ -23,5 +24,14 @@ app.get('/api/users', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 app.use(errorHandler)
+
+// Server Frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+}else{
+    app.get('/api/users', (req, res) => {
+    res.status(201).json({message: 'Welcome to the Support Desk API'})
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
