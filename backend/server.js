6b57735522d5e78f4377263defc0dc7fd2e06584
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-08-12 16:17:07 +0530

    Deploy 2

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,38 @@
 const path = require('path')
 const express = require('express')
 const colors = require('colors')
-const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const PORT = process.env.PORT || 8000
-// looks for the environment variable, if not there it takes 8000 as port
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
 
-//Connect to database
+// Connect to database
 connectDB()
 
 const app = express()
 
-//add middleware
-app.use(express.json())//this allows to send raw json
-app.use(express.urlencoded({extended: false}))//to accept the urlencoded form
-
-app.get('/api/users', (req, res) => {
-    res.status(201).json({message: 'Welcome to the Support Desk API'})
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
-app.use(errorHandler)
 
-// Server Frontend
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
-}else{
-    app.get('/api/users', (req, res) => {
-    res.status(201).json({message: 'Welcome to the Support Desk API'})
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
 }
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
