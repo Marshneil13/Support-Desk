5f2a40dd7a23c7b401419df4000e11151d0145c0
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-01-18 12:12:45 +0530

    cyclic pre-deployment modifications

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -11,28 +11,17 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
+//add middleware
+app.use(express.json()); //this allows to send raw json
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
-
-// Serve Frontend
-if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  // FIX: below code fixes app crashing on refresh in deployment
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the Support Desk API" });
-  });
-}
-
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
